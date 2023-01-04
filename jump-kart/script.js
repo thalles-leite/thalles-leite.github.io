86642eaa976b89262fc7b1b8a8610882912249eb
Thalles Leite <eng.thallesleite@gmail.com>
2023-01-04 09:48:56 -0300

    Alteração de imagem do player.

--- jump-kart/script.js
+++ jump-kart/script.js
@@ -371,7 +371,6 @@ const pararTudo = () => {
     clearInterval(verificaVelocidade);
     clearInterval(moverIntervalo)
     player.style.animationPlayState = 'paused';
-    player.style.backgroundImage = 'url(imagens/mario2.gif)'
     estado = 'stop';
     clearInterval(pontuacaoInterval);
     const resultado = info.filter(objeto => objeto.nome === Pname)
